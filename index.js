feb8f9a157ee668925f5c248c9e2892973cea712
Robert Drabant <rdrabant@resultsdelivered.com>
2021-12-08 18:45:20 -0800

    initial js and js classes

---
+++ index.js
@@ -0,0 +1,39 @@
+
+
+
+
+// shuffle an array code found here. 
+// https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
+
+  class Card {
+    constructor(suite, imageUrl, value) {
+      this.suite = suite;
+      this.imageUrl = imageUrl;
+      this.value = value;
+    }
+
+    isAce(){
+        if(this.value === -1){
+            return true;
+        }
+        return false;
+    }
+  }
